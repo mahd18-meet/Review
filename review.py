cd5c29577bb8131488c315f723748f033db55505
mahd18-meet <mqumseya@js-bethlehem.com>
2017-02-17 05:59:13 -0500

    This is for Review

---
+++ review.py
@@ -0,0 +1,16 @@
+#This is for review
+
+def hello_World(x = 3, y = 4):
+    #x = 3
+    for i in range(x):
+        print ("Hello World."* y )
+
+weather = "cold"
+if weather == "raining" or weather == "Raining":
+    print ("Umbrella")
+elif weather == "cold":
+    print ("The weather is cold")
+else:
+    print ("weather is clear")
+
+#and, not, !=, >, <, 
